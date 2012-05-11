c36f84a21a1b7b58d5073b539471d7ad86e842bb
Alex <alex@rallydev.com>
2012-05-11 08:58:04 -0600

    Trying to figure out where to put a custom card renderer

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
+             extend: 'Rally.app.App',
+         
+             launch: function() {
+                 var cardBoardConfig = {
+                     xtype: 'rallycardboard',
+                     types: ['User Story'],
+                     attribute: "ScheduleState",
+					 cardConfig: 'DrilldownCard'
+                 };
+         
+                 this.add(cardBoardConfig);
+             }
+			 
+         });
 
-    launch: function() {
-        //Write app code here
-		console.log('Hello World');
-    }
-});
+Ext.define('Rally.ui.cardboard.DrilldownCard', {
+	extend: 'Rally.ui.cardboard.ArtifactCard',
+	
+	buildHeader: function() {
+		alert("Marvelous!");
+	}
+})
+
+         Rally.launchApp('CustomApp', {
+             name: 'Cardboard with quick-add'
+
+         });

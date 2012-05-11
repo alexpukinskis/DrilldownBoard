6b03ce649d13d4406b6e9cc0aa8c33b4c5c07961
Charles <Ferentchak@gmail.com>
2012-05-11 09:15:18 -0600

    Adding custom card for AlexBan

---
+++ DrilldownCard.js
@@ -0,0 +1,8 @@
+Ext.define('Rally.ui.cardboard.DrilldownCard', {
+    extend: 'Rally.ui.cardboard.ArtifactCard',
+    alias:'widget.drilldowncard',
+
+    buildHeader: function() {
+        alert("Marvelous!");
+    }
+});
